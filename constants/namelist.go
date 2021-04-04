ae85e50ec2bc77a0508c9c05d2bed5a23f9bc5a0
rssujay <rssujay97@gmail.com>
2021-04-05 02:34:51 +0800

    Add unspecified user

--- constants/namelist.go
+++ constants/namelist.go
@@ -1,10 +1,11 @@
 package constants
 
 const (
-	Elston  string = "elston"
-	Tamelly string = "tamelly"
-	Matthew string = "matthew"
-	Sujay   string = "sujay"
-	ZiYun   string = "ziyun"
-	Denise  string = "denise"
+	Elston    string = "elston"
+	Tamelly   string = "tamelly"
+	Matthew   string = "matthew"
+	Sujay     string = "sujay"
+	ZiYun     string = "ziyun"
+	Denise    string = "denise"
+	Anonymous string = "anonymous"
 )
